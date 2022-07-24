eaa14c87a27bc4ebc309a1392d7340ca80089e95
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-07-25 00:30:50 +0530

    tabular updated

--- scientific_computing/04_regula_falsi.py
+++ scientific_computing/04_regula_falsi.py
@@ -3,6 +3,7 @@ Method of False Position
 also known as Regula-falsi
 """
 from typing import Callable, Any
+from tabulate import tabulate
 
 
 def regula_falsi(func: Callable[[float], float], point_1: float, point_2: float, err: float) -> Any:
@@ -14,19 +15,25 @@ def regula_falsi(func: Callable[[float], float], point_1: float, point_2: float,
         return None
     else:
         n: int = 0
-        print("n\t|a\t\t\t|b\t\t\t|f(a)\t\t|f(b)\t\t|root\t\t|f(root)\t|")
-        while abs(f_r := (func(root := (point_1 * (f_point_2 := func(point_2)) - point_2 * (f_point_1 := func(point_1))) / (f_point_2 - f_point_1)))) >= err:
+        s: list = []
+        while abs(f_r := (
+                func(root := (point_1 * (f_point_2 := func(point_2)) - point_2 * (f_point_1 := func(point_1))) / (
+                        f_point_2 - f_point_1)))) >= err:
             if f_r * f_point_1 < 0:
-                print(f"{n}\t|{point_1:0.5f}\t|{point_2:0.5f}\t|{f_point_1:0.5f}\t|{f_point_2:0.5f}\t|{root:0.5f}\t|{f_r:0.5f}\t|")
+                s.append([n, point_1, point_2, f_point_1, f_point_2, root, f_r])
                 point_2 = root
             elif f_r * f_point_2 < 0:
-                print(f"{n}\t|{point_1:0.5f}\t|{point_2:0.5f}\t|{f_point_1:0.5f}\t|{f_point_2:0.5f}\t|{root:0.5f}\t|{f_r:0.5f}\t|")
+                s.append([n, point_1, point_2, f_point_1, f_point_2, root, f_r])
                 point_1 = root
             n += 1
 
-        return root
+        return root, s
 
 
 if __name__ == '__main__':
     func: Callable[[float], float] = lambda x: x ** 3 - x ** 2 + 2
-    print(regula_falsi(func, -5.0, 5.0, 0.01))
+    result = regula_falsi(func, -5.0, 5.0, 0.01)
+    print(f"root is {result[0]}")
+    table = result[1]
+    header = ["n", "a", "b", "f(a)", "f(b)", "root", "f(root)"]
+    print(tabulate(table, header, tablefmt="github"))
