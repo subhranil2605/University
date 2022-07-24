ad0b89e26991fbca18e90bece88af51eca8dd89e
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-07-25 00:33:17 +0530

    updated

--- scientific_computing/07_newton_forward_interpolation.py
+++ scientific_computing/07_newton_forward_interpolation.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 plt.style.use('seaborn-whitegrid')
-
 
 x = np.arange(0.1, 0.6, 0.1)
 y = np.exp(2 * x)
 
 # finite difference
 h = x[1] - x[0]
+
 
 # fractorial
 def factorial(x):
@@ -30,10 +29,11 @@ def get_table(y):
 
     for i in range(1, size):
         for j in range(size - i):
-            coef[j][i] = coef[j + 1][i - 1] - coef[j][i-1]
+            coef[j][i] = coef[j + 1][i - 1] - coef[j][i - 1]
 
-    print(coef)  
+    print(coef)
     return coef
+
 
 # getting the table
 tabl = get_table(y)
@@ -47,28 +47,26 @@ def newton_forward(x_value, table, x, y):
         an = table[0][i] / (factorial(i) * np.power(h, (i)))
         for j in range(i):
             rk *= x_value - x[j]
-        
+
         result += rk * an
 
     return result
+
 
 x_val = 0.05
 result = newton_forward(x_val, tabl, x, y)
 
 print(f"Value of f({x_val}) = {result:0.6f}")
 
-
 # plotting
 
 x_new = np.linspace(-1, 2.1, 1000)
 y_new = newton_forward(x_new, tabl, x, y)
 
-plt.figure(figsize = (12, 8))
+plt.figure(figsize=(12, 8))
 plt.plot(x, y, 'ko', label="Given Points")
 plt.plot(x_new, y_new, label="Estimatd function")
-plt.plot(x_new, np.exp(2*x_new), '--r',label="Actual function")
-
-
+plt.plot(x_new, np.exp(2 * x_new), '--r', label="Actual function")
 
 plt.legend()
 ##plt.savefig('Newton_forward_interpolation.jpg')
--- scientific_computing/08_newton_backward_interpolation.py
+++ scientific_computing/08_newton_backward_interpolation.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 plt.style.use('seaborn-whitegrid')
-
 
 
 def factorial(x):
@@ -19,17 +17,17 @@ y = np.exp(2 * x)
 h = x[1] - x[0]
 
 
-
 def get_table(y):
     n = len(y)
     coef = np.zeros((n, n))
     coef[:, 0] = y
 
     for i in range(1, n):
-        for j in range(n-1, i-1, -1):
-            coef[j][i] = coef[j][i-1] - coef[j-1][i-1]
-    
+        for j in range(n - 1, i - 1, -1):
+            coef[j][i] = coef[j][i - 1] - coef[j - 1][i - 1]
+
     return coef
+
 
 tabl = get_table(y)
 
@@ -41,12 +39,11 @@ def newton_backward(x_value, table, x, y):
         rk = 1
         an = table[len(x) - 1][i] / (factorial(i) * np.power(h, (i)))
         # print(an)
-        for j in range(n-1, n-i-1, -1):
+        for j in range(n - 1, n - i - 1, -1):
             rk *= x_value - x[j]
         result += rk * an
 
     return result
-
 
 
 x_val = 0.37
@@ -54,15 +51,14 @@ result = newton_backward(x_val, tabl, x, y)
 
 print(f"Value of f({x_val}) = {result:0.6f}")
 
-
 # plotting
 
 x_new = np.linspace(-1, 2.1, 1000)
 y_new = newton_backward(x_new, tabl, x, y)
 
-plt.figure(figsize = (12, 8))
+plt.figure(figsize=(12, 8))
 plt.plot(x_new, y_new, label="Estimatd function")
-plt.plot(x_new, np.exp(2*x_new), '--r',label="Actual function")
+plt.plot(x_new, np.exp(2 * x_new), '--r', label="Actual function")
 plt.plot(x, y, 'ko', label="Given Points")
 plt.title("Newton Backward Interpolation")
 
