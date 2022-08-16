493f824d5fcd4e386c6765d9a377f1ffaf35fba5
Subhranil Sarkar <57196419+subhranil2605@users.noreply.github.com>
2022-08-16 11:15:53 +0530

    added

---
+++ Scientific Computing/scientific_computing/09_lagrange_interpolation.py
@@ -0,0 +1,48 @@
+"""
+Lagrange Interpolation
+Subhranil Sarkar
+"""
+from tabulate import tabulate
+
+
+def lagrange_func(x_values: list, y_values: list, x) -> int | float:
+    """
+    Lagrange Interpolation
+
+    :param x_values: list of the given data points
+    :param y_values: list of the other data points
+    :param x: value given
+    :raises AssertionError: if the length of the two lists do not match
+    :retuns: calculated value for x
+    """
+    
+    assert len(x_values) == len(y_values), "Length of the two lists should be same!!!"
+
+    # to store the result 
+    result = 0
+    
+    for i, y_val in enumerate(y_values):
+        l_i = y_val
+        for j in range(len(x_values)):
+            if j != i:
+                l_i *= (x - x_values[j]) / (x_values[i] - x_values[j])
+                
+        result += l_i
+
+    return result
+
+
+if __name__ == "__main__":
+    x_vals: list = [4, 12, 19]
+    y_vals: list = [1, 3, 4]
+    
+    x: int = 7
+    
+    table: list[list] = [[i, j] for i, j in zip(x_vals, y_vals)]
+            
+    y: int | float = lagrange_func(x_vals, y_vals, x)
+
+    print(tabulate(table, headers=["X", "Y"], showindex="always", tablefmt="pretty"))
+    
+    print(f"For X: {x} the value of Y: {y:0.6f}")
+    
---
+++ Scientific Computing/scientific_computing/tests/test_lagrange.py
@@ -0,0 +1,14 @@
+import pytest
+from 
+
+
+def test_lagrange_func():
+    x_values: list[int] = [4, 12, 19]
+    y_values: list[int] = [1, 3, 4]
+    assert 
+
+
+
+
+if __name__ == "__main__":
+    pytest.main()
